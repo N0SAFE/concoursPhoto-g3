e035bd883c71fa1d3b8f7867fcb68dd98eb10185
Yuki <merciert60@gmail.com>
2023-03-13 16:53:44 +0100

    création registration controller

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -1,49 +1,63 @@
 <?php
-
+ 
 namespace App\Entity;
-
+ 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-
+ 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 180, unique: true)]
-    private ?string $email = null;
-
-    #[ORM\Column]
-    private array $roles = [];
-
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
-
+    #[ORM\Column(type: 'integer')]
+    private $id;
+ 
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+ 
+    #[ORM\Column(type: 'string', length: 180)]
+    private $username;
+ 
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+ 
+    #[ORM\Column(type: 'string')]
+    private $password;
+ 
+     
+ 
     public function getId(): ?int
     {
         return $this->id;
     }
-
+ 
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
+ 
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
+ 
         return $this;
     }
-
+ 
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+ 
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+ 
+        return $this;
+    }
+ 
     /**
      * A visual identifier that represents this user.
      *
@@ -53,7 +67,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return (string) $this->email;
     }
-
+ 
     /**
      * @see UserInterface
      */
@@ -62,17 +76,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-
+ 
         return array_unique($roles);
     }
-
+ 
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
-
+ 
         return $this;
     }
-
+ 
     /**
      * @see PasswordAuthenticatedUserInterface
      */
@@ -80,14 +94,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         return $this->password;
     }
-
+ 
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
+ 
         return $this;
     }
-
+ 
     /**
      * @see UserInterface
      */
@@ -96,4 +110,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+ 
+    
 }
