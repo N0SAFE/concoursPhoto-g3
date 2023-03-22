bfbd741764a446e7718c2990bdf36edb45b59201
Théo <davigny.theo@gmail.com>
2023-03-23 00:41:21 +0100

    add error 404 page

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./views/auth/Login";
+import NotFound from "./views/error/NotFound";
 
 function Router() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 }
