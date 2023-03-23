628526c871104dba561435237e9a967962a65752
n0safe <sssebillemathis@gmail.com>
2023-03-23 14:48:50 +0100

    rework some code and add the atomis design in the components folder; add some config on vite to have alias on import for folder ./, ./src, ./src/assets, ./src/components, ./src/views

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useAuthContext } from "../contexts/AuthContext.jsx";
+import { useAuthContext } from "@/contexts/AuthContext.jsx";
 
 function login(setToken, { email, password }) {
     return new Promise((resolve, reject) => {
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import sassDts from 'vite-plugin-sass-dts'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,17 @@ export default defineConfig({
       enabledMode: ['development', 'production'],
     }),
   ],
+  resolve:{
+    alias:{
+      '@' : path.resolve(__dirname, './src'),
+      '@@': path.resolve(__dirname, './'),
+      '~': path.resolve(__dirname, './src'),
+      '~~': path.resolve(__dirname, './'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@views': path.resolve(__dirname, './src/views'),
+      '@components': path.resolve(__dirname, './src/components')
+    },
+  },
   server : {
     port: 8001,
   }
