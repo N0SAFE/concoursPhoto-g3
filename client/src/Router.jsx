f75ab63c9c8c651a3a269e7f91e8f78cf0709e80
Théo <davigny.theo@gmail.com>
2023-03-23 01:49:24 +0100

    add redirection from home to login

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -3,14 +3,18 @@ import Login from "./views/auth/Login";
 import UserList from "./views/BO/UserList";
 import UserEdit from "./views/BO/UserEdit";
 import UserCreate from "./views/BO/UserCreated.jsx";
+import BO from "./views/BO/BO";
+import Home from "./views/Home.jsx";
 
 function Router() {
     return (
         <Routes>
+            <Route path="/" element={<Home />} />
             <Route>
                 <Route path="/login" element={<Login />} />
             </Route>
             <Route path="/BO">
+                <Route path="" element={<BO />} />
                 <Route path="user" element={<UserList />} />
                 <Route path="user/:id" element={<UserEdit />} />
                 <Route path="user/create" element={<UserCreate />} />
