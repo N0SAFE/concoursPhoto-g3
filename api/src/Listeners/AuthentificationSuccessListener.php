cffad9e4538e586834d8965c6131c2e7188383c0
n0safe <sssebillemathis@gmail.com>
2023-03-24 11:37:24 +0100

    add jwt store on cookie http only

---
+++ api/src/Listeners/AuthentificationSuccessListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class AuthentificationSuccessListener
+{
+    private $secure = false;
+
+    public function __construct()
+    {
+    }
+
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
+    {
+        $response = $event->getResponse();
+        $data = $event->getData();
+
+        $token = $data['token'];
+        unset($data['token']);
+        unset($data['refresh_token']);
+        $event->setData($data);
+
+        $response->headers->setCookie(
+            new Cookie('BEARER', $token, new \DateTime('+1 day'), '/', null, $this->secure, true, 'strict')
+        );
+
+        $event->setData(['message' => 'loged in', 'code' => 200]);
+    }
+}
