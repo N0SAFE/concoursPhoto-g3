de59c1e86f2365b12ab524c636ee3cd25fdde8b8
n0safe <sssebillemathis@gmail.com>
2023-04-07 01:51:30 +0200

    add region and department on the user entity so the user is related to a region and department (this is done for the verification of the location of the user in the competition)

--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -3,14 +3,6 @@ import { useEffect } from "react";
 import {useAuthContext} from "@/contexts/AuthContext";
 
 export default function Home() {
-    const {isLogged} = useAuthContext();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!isLogged) {
-            navigate("/login");
-        }
-    }, []);
-
     return (
         <div>
             <h1>Home</h1>
