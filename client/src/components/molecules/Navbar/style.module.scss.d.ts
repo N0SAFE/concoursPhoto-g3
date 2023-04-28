ec8bbbffd19409d5cc83763520a7216f03c4eae1
n0safe <sssebillemathis@gmail.com>
2023-04-28 18:47:14 +0200

    add prettier at the root of the project and add a script

--- client/src/components/molecules/Navbar/style.module.scss.d.ts
+++ client/src/components/molecules/Navbar/style.module.scss.d.ts
@@ -1,8 +1,8 @@
 declare const classNames: {
-    readonly navbarContainer: 'navbarContainer';
-    readonly navbarResponsive: 'navbarResponsive';
-    readonly navbarStyle: 'navbarStyle';
-    readonly icon: 'icon';
-    readonly navbarShow: 'navbarShow';
+  readonly navbarContainer: "navbarContainer";
+  readonly navbarResponsive: "navbarResponsive";
+  readonly navbarStyle: "navbarStyle";
+  readonly icon: "icon";
+  readonly navbarShow: "navbarShow";
 };
 export = classNames;
