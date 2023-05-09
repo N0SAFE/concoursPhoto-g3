feccacdf5259539557d20bd3b14a78b96b7cd422
Yuki <merciert60@gmail.com>
2023-05-09 15:59:26 +0200

    Tiny MCE on endowments in competition

--- client/src/views/FO/competition/CompetitionEndowments/index.jsx
+++ client/src/views/FO/competition/CompetitionEndowments/index.jsx
@@ -2,10 +2,21 @@ import { useOutletContext, useParams } from 'react-router-dom';
 import Navlink from '@/components/molecules/Navlink/index.jsx';
 import PicturesAside from '@/views/FO/competition/PicturesAside/index.jsx';
 import style from './style.module.scss';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import useApiFetch from '@/hooks/useApiFetch.js';
+import { toast } from 'react-toastify';
+
 
 export default function () {
     const { competition } = useOutletContext();
-
+    const apiFetch = useApiFetch();
+    const editorRef = useRef(null);
+    const log = () => {
+      if (editorRef.current) {
+        console.log(editorRef.current.getContent());
+      }
+    };
     const competitionRouteList = [
         { content: 'Le concours', to: '' },
         { content: 'Règlement', to: '/rules' },
@@ -14,7 +25,24 @@ export default function () {
         { content: 'Les photos', to: '/pictures' },
         { content: 'Résultats', to: '/results' },
     ];
-
+    function updateCompetition() {
+        const res = apiFetch(
+            `/competitions/${competition.id}`,
+            {
+                method: 'PATCH',
+                body: JSON.stringify({ endowments:editorRef.current.getContent()}),
+                headers: {
+                    'Content-Type':
+                        'application/merge-patch+json',
+                },
+            }
+        )
+        toast.promise(res, {
+            pending: 'Mise à jour en cours',
+            success: 'Mise à jour effectuée',
+            error: 'Erreur lors de la mise à jour',
+        })
+    }
     return (
         <div>
             <div className={style.endowmentsContainer}>
@@ -23,14 +51,30 @@ export default function () {
                         base="/competition/:id"
                         list={competitionRouteList}
                     />
-                    <p className={style.description}>
-                        {competition.endowments}
-                    </p>
-                    <div>
-                   
+                    <div className={style.description} dangerouslySetInnerHTML={{ __html : competition.endowments}}>
+                        
                     </div>
-                   
+                    <Editor
+                    onInit={(evt, editor) => editorRef.current = editor}
+                    initialValue={competition.endowments}
+                        init={{
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                        'insertdatetime', 'table', 'code', 'help', 'wordcount','emoticons','charmap', 'insertdatetime'
+                            ],
+                        toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help' + ' | emoticons' + ' insertdatetime | code | preview',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+                    />
+                    <button style={{ borderRadius: "10%" }} onClick={updateCompetition}>Editer</button>
                 </div>
+                
                 <PicturesAside requestType={'last-pictures-obtained-votes'} />
             </div>
         </div>
