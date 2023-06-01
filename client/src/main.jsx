8e238f925f80a930f66df377250a01d7f55c3d1a
n0safe <sssebillemathis@gmail.com>
2023-06-01 23:15:17 +0200

    remove the top level await on main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,43 +8,11 @@ if (!import.meta.env.VITE_API_URL) {
 
 console.debug = () => {};
 
-if (import.meta.env.MODE === 'development') {
-    console.warn('You are running the app in development mode');
-    console.debug = console.log;
-    const preApp = ReactDOM.createRoot(document.getElementById('root'));
-    preApp.render(
-        <div
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                zIndex: 999,
-                backgroundColor: 'white',
-                display: 'flex',
-                justifyContent: 'center',
-            }}
-        >
-            <h1>developement healthcheck...</h1>
-        </div>
-    );
-
-    const data = await (async () => {
-        try {
-            return await fetch(import.meta.env.VITE_API_URL + '/healthcheck', {
-                method: 'GET',
-            }).then(r => r.json());
-        } catch (e) {
-            return await e;
-        }
-    })();
-
-    if (data.status === 'ok') {
-        console.log('healthcheck ok');
-    } else {
-        console.error('healthcheck failed');
-        console.error(data);
+async function main() {
+    if (import.meta.env.MODE === 'development') {
+        console.warn('You are running the app in development mode');
+        console.debug = console.log;
+        const preApp = ReactDOM.createRoot(document.getElementById('root'));
         preApp.render(
             <div
                 style={{
@@ -56,41 +24,80 @@ if (import.meta.env.MODE === 'development') {
                     zIndex: 999,
                     backgroundColor: 'white',
                     display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
+                    justifyContent: 'center',
                 }}
             >
-                <h1>healthcheck failed</h1>
-                <div 
+                <h1>developement healthcheck...</h1>
+            </div>
+        );
+
+        const data = await (async () => {
+            try {
+                return await fetch(
+                    import.meta.env.VITE_API_URL + '/healthcheck',
+                    {
+                        method: 'GET',
+                    }
+                ).then(r => r.json());
+            } catch (e) {
+                return await e;
+            }
+        })();
+
+        if (data.status === 'ok') {
+            console.log('healthcheck ok');
+        } else {
+            console.error('healthcheck failed');
+            console.error(data);
+            preApp.render(
+                <div
                     style={{
-                        flexGrow: 1,
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        zIndex: 999,
+                        backgroundColor: 'white',
                         display: 'flex',
-                        justifyContent: 'center',
+                        flexDirection: 'column',
                         alignItems: 'center',
                     }}
                 >
-                <pre
-                    style={{
-                        overflow: 'auto',
-                        color: 'red',
-                        backgroundColor: 'black',
-                        padding: '1rem',
-                        borderRadius: '1rem',
-                    }}
-                >
-                    {JSON.stringify(data, null, 2)}
-                </pre>
+                    <h1>healthcheck failed</h1>
+                    <div
+                        style={{
+                            flexGrow: 1,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <pre
+                            style={{
+                                overflow: 'auto',
+                                color: 'red',
+                                backgroundColor: 'black',
+                                padding: '1rem',
+                                borderRadius: '1rem',
+                            }}
+                        >
+                            {JSON.stringify(data, null, 2)}
+                        </pre>
+                    </div>
                 </div>
-            </div>
-        );
-        throw new Error('healthcheck failed');
+            );
+            throw new Error('healthcheck failed');
+        }
+
+        preApp.unmount();
     }
 
-    preApp.unmount();
+    ReactDOM.createRoot(document.getElementById('root')).render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+main();
