63656ee670a7cf74fab18e0a578481c43c68255e
Théo <davigny.theo@gmail.com>
2023-06-04 01:44:27 +0200

    add style for 404 page not found

--- client/src/views/error/NotFound/index.jsx
+++ client/src/views/error/NotFound/index.jsx
@@ -1,7 +1,11 @@
+import style from './style.module.scss';
+import Icon from "@/components/atoms/Icon";
+
 export default function NotFound() {
     return (
-        <div>
-            <h1>404 Not Found</h1>
+        <div className={style.notFoundContainer}>
+            <Icon icon={"warning"} size={100} />
+            <h1>404 Page Not Found</h1>
             <p>Sorry, the page you were trying to view does not exist.</p>
         </div>
     );
